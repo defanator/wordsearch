1880773cee92aadde8c281147acfef49433c5e16
Andrei Belov <defanator@gmail.com>
2024-04-27 19:44:11 +0400

    Add examples directory to lint amd fmt targets

--- examples/place_and_discover.py
+++ examples/place_and_discover.py
@@ -9,17 +9,18 @@ from wordsearch import WordsearchBoard
 
 ws = WordsearchBoard()
 
-words = ['BEAR', 'HAMSTER', 'GOOSE', 'MONKEY', 'FOX']
+words = ["BEAR", "HAMSTER", "GOOSE", "MONKEY", "FOX"]
 
 for word in words:
     ws.place_word(word)
 
+print("Board:")
 ws.print_board()
 
 for word in words:
     empty_grid = ws.empty_grid()
-    word_found = ws.exists(word)
+    word_found = ws.exists(word, res_board=empty_grid)
 
-    print(f'Searching for {word}: {word_found}')
-    #if rc:
-    #    ws.print_board(grid=empty_grid)
+    print(f"Searching for {word}: {word_found}")
+    if word_found:
+        ws.print_board(grid=empty_grid)
