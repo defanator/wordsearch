4065c19182bafeba6c6c75de245c85d254767e48
Andrei Belov <defanator@gmail.com>
2024-04-27 19:58:29 +0400

    Made example more verbose

--- examples/place_and_discover.py
+++ examples/place_and_discover.py
@@ -5,22 +5,35 @@
 Example of creating a board, placing some words, and searching for those
 """
 
+import sys
 from wordsearch import WordsearchBoard
 
-ws = WordsearchBoard()
 
-words = ["BEAR", "HAMSTER", "GOOSE", "MONKEY", "FOX"]
+def main():
+    """
+    Entry point
+    """
+    words = ["BEAR", "HAMSTER", "GOOSE", "MONKEY", "FOX"]
 
-for word in words:
-    ws.place_word(word)
+    ws = WordsearchBoard()
 
-print("Board:")
-ws.print_board()
+    for word in words:
+        if ws.place_word(word):
+            print(f":) word {word} placed on board")
+        else:
+            print(f":( word {word} could not be placed on board")
 
-for word in words:
-    empty_grid = ws.empty_grid()
-    word_found = ws.exists(word, res_board=empty_grid)
+    print("\nBoard:")
+    ws.print_board()
 
-    print(f"Searching for {word}: {word_found}")
-    if word_found:
-        ws.print_board(grid=empty_grid)
+    for word in words:
+        empty_grid = ws.empty_grid()
+        word_found = ws.exists(word, res_board=empty_grid)
+
+        print(f"\nSearching for {word}: {word_found}")
+        if word_found:
+            ws.print_board(grid=empty_grid)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
